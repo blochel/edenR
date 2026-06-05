3bb3ad3f082547865d0c8d84712d7d8491427c1c
Alexander Blochel <alexanderblochel@ufl.edu>
2026-06-05 11:13:47 -0400

    /regions went missing

--- R/eden_covariates.R
+++ R/eden_covariates.R
@@ -362,7 +362,7 @@ get_eden_covariates <- function(
 get_eden_depths <- function(level = "subregions",
                             eden_path = file.path("Water"),
                             years = available_years(eden_path),
-                            boundaries_path = "https://raw.githubusercontent.com/weecology/EvergladesWadingBird/refs/heads/main/SiteandMethods/",
+                            boundaries_path = "https://raw.githubusercontent.com/weecology/EvergladesWadingBird/refs/heads/main/SiteandMethods/regions/",
                             colony_buffers = default_colony_buffers()) {
   eden_data_files <- list.files(eden_path, pattern = "_depth.nc", full.names = TRUE)
   boundaries <- get_boundaries(boundaries_path, level, colony_buffers)
@@ -396,7 +396,7 @@ get_eden_depths <- function(level = "subregions",
 }
 
 #' @name reshape_star
-#'
+
 #' @title Reshapes star object to dataframe
 #'
 #' @param data data to reshape
